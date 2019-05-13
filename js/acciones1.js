e6bc932d957bc233e45494a3d038cd2e5eb3e1d6
brianhernandezram13 <38222614+brianhernandezram13@users.noreply.github.com>
2019-05-13 17:28:12 -0500

    f

--- js/acciones1.js
+++ js/acciones1.js
@@ -1,27 +1,12 @@
-//JavaScript Document app6
+// JavaScript Document
 
-$(document).ready(function (e) {
-	document.addEventListener("deviceready",function(){
-		
-		$('#izquierda').on("swipeleft",function(){
-			navigator.notification. alert("Deslizo a la izquierda",function(){"Aplicacion ","Aceptar"});
-		});
-		
-		$('#derecha').on("swiperight",function(){
-	navigator.notification. confirm("¿Que quieres hacer?",function(opt){
-		switch(opt)
-		{
-			case 1:
-			navigator.notification.beep(1);
-			break;
-			
-			case 2:
-			navigator.notification.vibrate(1000);
-			break;
-		}
-	},"Aplicacion ","Beep,Vibrar,Cancelar");
+$(document).ready(function (e){
+	document .addEventListener ("deviceready",function(){
+		$('#disp table td').eq(3).text(device.model);
+		$('#disp table td').eq(5).text(device.cordova);
+		$('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
+	},false);//ready device
 	
-		});
-	}.false);
-});
-
+});//document
