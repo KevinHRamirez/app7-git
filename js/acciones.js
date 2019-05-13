1467dac4bc3d10748378562285d4ff0f42b79d89
brianhernandezram13 <38222614+brianhernandezram13@users.noreply.github.com>
2019-05-13 17:49:50 -0500

    h

--- js/acciones.js
+++ js/acciones.js
@@ -25,3 +25,31 @@ $(document).ready(function (e) {
 	}.false);
 });
 
+//JavaScript 
+
+$(document).ready(function(e){
+document.addEventListener("deviceready",function(){
+
+$('#beep').tap(function(){
+navigator.notification.beep(3);
+});//tap beep
+
+$('#vibrar').tap(function(){
+navigator.notification.vibrate(1000);
+});//tap vibrar
+
+},false); //deviceready
+}); //ready
+
+// JavaScript Document
+
+$(document).ready(function (e){
+	document .addEventListener ("deviceready",function(){
+		$('#disp table td').eq(3).text(device.model);
+		$('#disp table td').eq(5).text(device.cordova);
+		$('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
+	},false);//ready device
+	
+});//document
